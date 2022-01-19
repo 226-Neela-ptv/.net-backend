9146803810ff345f15ad5239913ab1442a71e6c1
226-Neela-ptv <97968284+226-Neela-ptv@users.noreply.github.com>
2022-01-19 09:45:58 +0530

    This is .Net backend repos

---
+++ ConsoleApp4/ConsoleApp4/EmployeeName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ConsoleApp4
+{
+    class program
+    {
+     static void main(string[] args)
+        {
+            EmployeeName = "neela";
+             Console.WriteLine(EmployeeName);
+      
+
+        }
+    }
+}
